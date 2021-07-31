ea3d106a43a240fa8701a91fbf473f1fd0834f7e
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-07-31 16:54:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -141,7 +141,44 @@ if(pcscore ==4){
    }   
 }
 
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
 
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas = parent('canvas');
+
+	instializeInSetup(mario);
+
+     video = createCapture(VIDEO);
+	 video.size(800,400);
+	 video.parent('game_console');
+
+	 poseNet = ml5.poseNet(video, modelLoaded);
+	 poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log('Model Loaded!');
+}
+
+function gotPoses()
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+}
+
+function draw() 
+{
+	game()
+}
 //width height of canvas speed of ball 
 function models(){
     textSize(18);
